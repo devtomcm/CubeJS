4c227ee1992d69a441f79dfb11e08205705c1dbf
tom <devtomcm@gmail.com>
2022-10-27 22:44:30 +0900

    init

---
+++ CubeJS/src/test/java/com/twincube/CubeJS/CubeJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twincube.CubeJS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubeJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
